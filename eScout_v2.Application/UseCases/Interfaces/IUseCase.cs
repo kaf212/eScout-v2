cacb2560c6a36fb188706a3d43e82b647f2884cd
Jan Atzgerstorfer <115266756+kaf212@users.noreply.github.com>
2026-09-23 16:33:26 +0200

    ESC-2: UseCase Interfaces

---
+++ eScout_v2.Application/UseCases/Interfaces/IUseCase.cs
@@ -0,0 +1,21 @@
+﻿namespace eScout_v2.Application.UseCases.Interfaces;
+
+public interface IUseCase<in TInput, TOutput> where TInput : class
+{
+    public ValueTask<TOutput> ExecuteAsync(TInput input, CancellationToken cancellationToken);
+}
+
+public interface IUseCase<TOutput>
+{
+    public ValueTask<TOutput> ExecuteAsync(CancellationToken cancellationToken);
+}
+
+public interface IUseCaseVoid<in TInput> where TInput : class
+{
+    public ValueTask ExecuteAsync(TInput input, CancellationToken cancellationToken);
+}
+
+public interface IUseCaseVoid
+{
+    public ValueTask ExecuteAsync(CancellationToken cancellationToken);
+}
